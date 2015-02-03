76497d5b57db9c349a026a4fd4fe432bd8a39435
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-02-03 13:26:37 -0500

    show ui feedback after selecting answer in quiz

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -5,21 +5,22 @@
     var $3 = $('#3');
     var $4 = $('#4');
     var $next = $('#next');
-    var $submit = $('#submit');
+
+    var WRONG_CLASS = 'btn-danger';
+    var RIGHT_CLASS = 'btn-success';
 
     var setup = function (test) {
-
-        $q.html(test.question.q);
-
-        $1.html(test.answers[0].text);
-        $2.html(test.answers[1].text);
-        $3.html(test.answers[2].text);
-        $4.html(test.answers[3].text);
+        $q.html(test.question.q).data(test);
+        $1.html(test.answers[0].text).data(test.answers[0]);
+        $2.html(test.answers[1].text).data(test.answers[1]);
+        $3.html(test.answers[2].text).data(test.answers[2]);
+        $4.html(test.answers[3].text).data(test.answers[3]);
     };
 
     var loadTest = function () {
         $next.hide();
-        $submit.show();
+        $('.' + WRONG_CLASS).removeClass(WRONG_CLASS);
+        $('.' + RIGHT_CLASS).removeClass(RIGHT_CLASS);
 
         $.ajax({
             url: '/test',
@@ -35,15 +36,24 @@
         });
     };
 
-    $next.on('click', function () {
-        loadTest();
+    $('#answers > button').on('click', function () {
+        var $btn = $(this);
+        var answerData = $btn.data();
+        var questionData = $q.data();
+
+        if (questionData.question._id !== answerData._id) {
+            $btn.addClass(WRONG_CLASS);
+        }
+
+        $('button').filter(function (i, btn) {
+            return $(btn).data('_id') === questionData.question._id;
+        }).addClass(RIGHT_CLASS);
+
+        $next.show();
     });
 
-    $submit.on('click', function () {
-        console.log('check answer');
-        console.log('show correct');
-        $submit.hide();
-        $next.show();
+    $next.on('click', function () {
+        loadTest();
     });
 
     loadTest();
